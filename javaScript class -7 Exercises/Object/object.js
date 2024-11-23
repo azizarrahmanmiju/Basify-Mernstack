4c00a72b3074fd1f833a2069e88734f4beeaeacf
azizarrahman <azizarrahman01032003@gmail.com>
2024-11-23 14:27:14 +0600

    function in Object

--- javaScript class -7 Exercises/Object/object.js
+++ javaScript class -7 Exercises/Object/object.js
@@ -3,6 +3,12 @@ let Person = {
     name: "John",
     age: 30,
     height: 180,
+    lastname: "Doe",
+    nameoutput: function () {
+        console.log(this.name + this.lastname)
+    }
 }
 
-console.log(Person.name); 
+console.log(Person.name);
+console.log(Person.age);
+Person.nameoutput(); 
