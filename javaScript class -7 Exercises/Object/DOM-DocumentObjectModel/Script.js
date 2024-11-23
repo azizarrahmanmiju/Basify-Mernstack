57c2539ebed2c431899eb987c45b4254f468cbb8
azizarrahman <azizarrahman01032003@gmail.com>
2024-11-23 15:26:06 +0600

    Set Text In Html

--- javaScript class -7 Exercises/Object/DOM-DocumentObjectModel/Script.js
+++ javaScript class -7 Exercises/Object/DOM-DocumentObjectModel/Script.js
@@ -1,4 +1,8 @@
 const mybutton = document.getElementById("myButton");
+const p = document.getElementById("myParagraph");
+
+
 mybutton.addEventListener("click", () => {
-    alert("Button clicked");
+    // alert("Button clicked");
+    p.textContent = "Btn Clicked";
 })
