3e5625fd3962fc7cafc4e1199a07acf5778e2828
azizarrahman <azizarrahman01032003@gmail.com>
2024-11-23 14:56:18 +0600

    Nested Array In Object

--- javaScript class -7 Exercises/Object/object.js
+++ javaScript class -7 Exercises/Object/object.js
@@ -6,13 +6,27 @@ let Person = {
     lastname: "Doe",
     nameoutput: function () {
         console.log(this.name + this.lastname)
-    }
+    },
+    phones: [ //Nested Array In Object
+        "012456854", "0214578635"
+    ]
 }
 
+//Read Object
 console.log(Person.name);
 console.log(Person.age);
 Person.nameoutput();
+
 // add Some data In the object
 Person.country = "USA"; //or
 Person["country"] = "USA";
 console.log(Person);
+
+//Modifie the data in the object
+Person.age = 31;
+Person.name = "Azizar"; //or
+Person["name"] = "Azizar";
+console.log(Person);
+
+// read Nested Array In Object
+console.log(Person.phones[0]);
